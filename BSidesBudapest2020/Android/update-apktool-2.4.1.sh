1f42ab2b1692476f7876b68d691978458ec22d8c
lopesg <37779491+lopesg@users.noreply.github.com>
2020-06-30 21:30:18 +0100

    Update apktool script
    Perform execution permissions before to move the files

--- BSidesBudapest2020/Android/update-apktool-2.4.1.sh
+++ BSidesBudapest2020/Android/update-apktool-2.4.1.sh
@@ -8,11 +8,10 @@ wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/
 # Download apktool 2.4.1.jar
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar
 
-echo "Move files to $DEST"
+echo "Give execution permission"
+chmod a+x apktool
+chmod a+x apktool_2.4.1.jar
 
+echo "Move files to $DEST"
 sudo mv apktool $DEST/apktool
 sudo mv apktool_2.4.1.jar $DEST/apktool.jar
-
-echo "Give execution permission"
-sudo chmod a+x $DEST/apktool
-sudo chmod a+x $DEST/apktool.jar
